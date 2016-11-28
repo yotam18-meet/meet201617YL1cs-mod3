c46eeac35a8ea0894577f75ec40fa422508bc33b
yotam18-meet <yotamasarl7@gmail.com>
2016-11-28 19:33:30 +0200

    added stuff

--- UserAccount.py
+++ UserAccount.py
@@ -12,7 +12,8 @@ class UserAccount :
         """
         self.username=username
         #Complete the __init__ method
-
+        self.password=password
+        self.secret=secret
     def print_secret(self,password_attempt):
         """
         Checks user input, password_attempt - if this matches the user's
@@ -23,3 +24,7 @@ class UserAccount :
         #Complete this method - check the input variable, password_attempt,
         #against the password for this user (the value of the instance variable, password).
         #If, and only if, there's a match, then print the user's secret to the screen.
+        if password_attempt == self.password:
+            print(self.secret)
+            
+        
