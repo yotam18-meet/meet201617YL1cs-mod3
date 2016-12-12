f97014615fa5dc99c174a15816515f890a26a90a
yotam18-meet <yotamasael7@gmail.com>
2016-12-12 18:08:19 +0200

    Created the Student class

---
+++ student.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(self, name, hometown, age, height, favourite_ice_cream_flavour):
+        self.name = name
+        self.hometown = hometown
+        self.age = age
+        self.height = height
+        self.favourite_ice_cream_flavour = favourite_ice_cream_flavour
+    def print_summary:
+        print(str(self.name)+" is "+str(self.age)+" years old, and was born in "+/
+              str(self.homewtown)+". Did you know "+ str(self.name)+" is "+/
+              str(self.height)+ " tall? And they absolutely LOVE "+/
+              str(self.favourite_ice_cream_flavour)+ " ice cream!")
+
+    def get_giraffe_gap:
+        print("There are " + str(int(500)-int(self.hegiht))+ " cm between "+/
+              str(self.name)+ "and an average giraffe.")
+        
