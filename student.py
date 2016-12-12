75839b3f57194579b76dd755739dbdf89cf22084
yotam18-meet <yotamasael7@gmail.com>
2016-12-12 19:03:29 +0200

    added function calling code

--- student.py
+++ student.py
@@ -5,13 +5,9 @@ class Student:
         self.age = age
         self.height = height
         self.favourite_ice_cream_flavour = favourite_ice_cream_flavour
-    def print_summary:
-        print(str(self.name)+" is "+str(self.age)+" years old, and was born in "+/
-              str(self.homewtown)+". Did you know "+ str(self.name)+" is "+/
-              str(self.height)+ " tall? And they absolutely LOVE "+/
-              str(self.favourite_ice_cream_flavour)+ " ice cream!")
+    def print_summary(self):
+        print(str(self.name)+" is "+str(self.age)+" years old, and was born in "+ str(self.hometown)+". Did you know "+ str(self.name)+" is "+ str(self.height)+ "cm tall? And they absolutely LOVE "+ str(self.favourite_ice_cream_flavour)+ " ice cream!")
 
-    def get_giraffe_gap:
-        print("There are " + str(int(500)-int(self.hegiht))+ " cm between "+/
-              str(self.name)+ "and an average giraffe.")
+    def get_giraffe_gap(self):
+        print("There are " + str(float(500)-float(self.height))+ " cm between "+ str(self.name)+ " and an average giraffe.")
         
---
+++ student_test.py
@@ -0,0 +1,4 @@
+from student import Student
+yotam=Student("Yotam", "Jerusalem", "15", "1.68", "Vanilla")
+yotam.print_summary()
+yotam.get_giraffe_gap()
